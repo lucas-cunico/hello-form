7d9a94ea9b2af5ea3c0bc40ebaf2c46e6b1679d4
Lucas <lucas-cunico@hotmail.com>
2020-03-24 17:07:01 -0300

    maybe

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import ReactTooltip from "react-tooltip";
+
 
 class Home extends Component {
     render() {
         return (
             <main className="height-fix d-flex justify-content-center align-items-center">
-                <Link to="/form" className="btn btn-lg btn-secondary">Cadastro</Link>
+                <span data-tip data-for="happyFace">
+                <Link to="/form"  className="btn btn-lg btn-secondary">Cadastro</Link>
+                </span>
+                <ReactTooltip id="happyFace" type="error">
+                  <span>Show happy face</span>
+                </ReactTooltip>
             </main>
         );
     }
